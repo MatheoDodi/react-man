09a381cbb53eaee04b8c7eca4442d557f39ce9cd
MatthewDodi <matthew.dodi@gmail.com>
2019-12-08 22:13:09 -0800

    Included addition instructions for service-worker

--- src/index.js
+++ src/index.js
@@ -7,4 +7,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-serviceWorker.register();
+// You can enable the service worker by changing the method below
+// to .register(). Please make sure to also uncomment the necessary
+// code inside the public/index.html file
+serviceWorker.unregister();
